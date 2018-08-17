c85eb13c09b44ab65ced69921de45598fe247a06
rmartinezm <robertomtz@ciencias.unam.mx>
2018-08-17 02:27:31 -0500

    Added: predN

--- Practica01/src/naturalNumbers.hs
+++ Practica01/src/naturalNumbers.hs
@@ -10,8 +10,19 @@ toNat a = if a `mod` 2 == 0
 -- succ. Obtiene el sucesor de un n ́umero Nat.
 succN :: Nat -> Nat
 succN Zero = O Zero
-succN (O Zero) = (D (O Zero))
-succN (O (O x)) = (D (succN (O x)))
-succN (O (D x)) = (D (O x))
-succN (D (O x)) = (O (O x))
-succN (D (D x)) = (O (D x))
+succN (D Zero) = O Zero
+succN (O Zero) = D (O Zero)
+succN (O (O x)) = D (succN (O x))
+succN (O (D x)) = D (O x)
+succN (D (O x)) = O (O x)
+succN (D (D x)) = O (D x)
+
+-- pred. Obtiene el predecesor de un n ́umero Nat.
+predN :: Nat -> Nat
+predN Zero = error "Zero is the smallest natural"
+predN (D Zero) = error "Zero is the smallest natural"
+predN (O Zero) = Zero
+predN (D (D x)) = O (O (predN x))
+predN (D (O x)) = O (D x)
+predN (O (D x)) = D (D x)
+predN (O (O x)) = D (O x)
